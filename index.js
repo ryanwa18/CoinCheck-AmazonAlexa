698a553d3a03c8946e80c77f43439bcc6b08a9f2
Ryan Ward <ryanwa18@vt.edu>
2018-01-22 19:27:28 -0500

    Uses Coinmarketcap API
    I updated the api to coinmarketcap.com to allow users to ask for the price of any cryptocurrency on the website.

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
+/**
+ * @author Ryan Ward
+ * @version 1.0
+ * 
+ * This is a small alexa skill that can tell a user the price of several different cryptocurrencies.
+ */
+
 "use strict";
  
 // Include the Alexa SDK
 var Alexa = require("alexa-sdk");
-var http = require("http");
+var https = require("https");
  
 // The handlers object tells Alexa how to handle various actions
 var handlers = {
     "LaunchRequest": function () {
-      this.emit(":tell", "Welcome to Bitcoin Price Checker"); // Create speech output. This is what Alexa will speak back when the user says "Open bitcoin checker"
+      this.emit(":tell", "Welcome to Crypto Check!"); // Create speech output. This is what Alexa will speak back when the user says "Open bitcoin checker"
     },
     "PriceIntent": function () {
-        //https://blockchain.info/ticker- URL for the api call to get current price.
-        var options = {
-            host: 'blockchain.info',
-            port: 80,
-            method: 'GET',
-            path: '/ticker'
+        var crypto = this.event.request.intent.slots.cryptocurrency.value;
+        if (typeof(crypto) != "undefined") {
+            crypto = crypto.toLowerCase();
+            var path = "https://api.coinmarketcap.com/v1/ticker/" + crypto + "/";
+            //https://api.coinmarketcap.com/v1/ticker/ URL for the api call to get current price.
+            var req = https.get(path, res => {
+                res.setEncoding('utf8');
+                var returnData = "";
+                
+                res.on('data', chunk => {
+                    returnData =  returnData + chunk;
+                });
+                
+                res.on('end', () => {
+                    var result = JSON.parse(returnData);
+                    var price = result[0].price_usd;
+                    // Ensures that a cryptocurrency was spoken by the user.
+                    this.emit(":tell", "The current price of " + crypto + " is " + price + " USD");
+                    this.emit(":responseReady");
+                });
+            });
+            
+            req.end();
         }
-        var req = http.request(options, res => {
-            res.setEncoding('utf8');
-            var returnData = "";
-            
-            res.on('data', chunk => {
-                returnData =  returnData + chunk;
-            });
-            
-            res.on('end', () => {
-                var result = JSON.parse(returnData);
-                var btcPrice = result.USD.last;
-                this.emit(":tell", "The current price of Bitcoin is " + btcPrice + " USD"); // Calls the SayWelcomeMessage handler. This is what Alexa will speak back when the user says "Ask bitcoin checker to say the price"
-                this.emit(":responseReady");
-            });
-        })
-        
-        req.end();
+        else {
+            this.emit(":tell", "I'm sorry, I didn't get that. Could you please ask the question again?");
+            this.emit(":responseReady");
+        }
      
     }
 };
